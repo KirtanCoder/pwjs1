af9e408d968b62d88edb4a7a04c203e4093fd0e9
KirtanCoder <kirtankrpc@gmail.com>
2024-07-05 06:47:00 +0000

    new variables

---
+++ basic1/consoles.js
@@ -0,0 +1,3 @@
+console.log("hc")
+console.error("hc")
+console.log("hc")
---
+++ basic1/variiables.js
@@ -0,0 +1,7 @@
+// variables - let 
+// constants - const
+
+ const email = "kirtan@gmail.com"
+ let useremail = "kirtan@gmail.com"
+
+ console.log(email)

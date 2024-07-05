b49a239e3b0078dd48c2b13472f96161e9968649
KirtanCoder <kirtankrpc@gmail.com>
2024-07-05 18:08:48 +0000

    new added

---
+++ basic1/array.js
@@ -0,0 +1,30 @@
+// const mArray = [1,2,3]
+// const myarray = new Array(1,2,3,5)
+
+// console.log(mArray[0])
+
+const dcheroes = ["batsman", 'superman', "flash","Gl", ""]
+
+// console.log(dcheroes)
+
+// dcheroes.push("kirtan")
+// console.log(dcheroes)
+
+// const lasthero = dcheroes.pop()
+
+// console.log(dcheroes)
+
+// dcheroes.unshift("aquaman")
+// console.log(dcheroes)
+
+// console.log(myarray.toString())
+
+// console.log(dcheroes.toString())
+// console.log(myarray.join("-"))
+
+console.log(dcheroes.slice(1,3))
+console.log(dcheroes)
+
+console.log(dcheroes.splice(1,3))
+
+console.log(dcheroes)

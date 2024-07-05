6cfacb5f2c3494a450bb82a457d0b49d598a4496
KirtanCoder <kirtankrpc@gmail.com>
2024-07-05 17:41:49 +0000

    some change

--- basic1/conversion.js
+++ basic1/conversion.js
@@ -1,57 +1,74 @@
-let bank = "100"
+// let bank = "100"
 
-let Bankbalance = parseInt(bank)
+// let Bankbalance = parseInt(bank)
 
-console.log(bank)
+// console.log(bank)
 
-console.log(Bankbalance)
+// console.log(Bankbalance)
 
-let bam = 'abc'
+// let bam = 'abc'
 
-let bam1 = Number(bam)
+// let bam1 = Number(bam)
 
-console.log(bam)
-console.log(bam1)
+// console.log(bam)
+// console.log(bam1)
 
-// console.log(Nan == Nan)  -->  False
+// // console.log(Nan == Nan)  -->  False
 
-// console.log(5 + undefined)  -->  Nan
+// // console.log(5 + undefined)  -->  Nan
 
-// console.log( 5 + null) -->  5
+// // console.log( 5 + null) -->  5
 
-// console.log()  --->  not defined
+// // console.log()  --->  not defined
 
-const Name  = "kirtan"
+// const Name  = "kirtan"
 
-const name1 = 'kumar'
+// const name1 = 'kumar'
 
-console.log(Name + ' ' + name1)
+// console.log(Name + ' ' + name1)
 
-console.log(`kirtan ${Name} kumar ${name1}`)
+// console.log(`kirtan ${Name} kumar ${name1}`)
 
-const username = String('kirtan')
+// const username = String('kirtan')
 
-console.log(username.length)
+// console.log(username.length)
 
-console.log(username.charAt(0))
+// console.log(username.charAt(0))
 
-console.log(username.substring(0,4))
+// console.log(username.substring(0,4))
 
-let email = "kirtan@.com"
+// let email = "kirtan@.com"
 
-console.log(email.trim())
+// console.log(email.trim())
 
-//trim
+// //trim
 
-let emailTwo = "hc@pw"
+// let emailTwo = "hc@pw"
 
-console.log(emailTwo.replace('hc', 'kirtans'))
-console.log(username.includes('kirtan'))
+// console.log(emailTwo.replace('hc', 'kirtans'))
+// console.log(username.includes('kirtan'))
 
 
-//convert lowercase to uppercase
+// //convert lowercase to uppercase
 
-let user =" kirtan.com"
-let user2 ="ayush.com"
+// let user =" kirtan.com"
+// let user2 ="ayush.com"
 
-console.log(user.toUpperCase())
+// console.log(user.toUpperCase())
+
+
+const num = new Number(5.34443)
+
+console.log(num.toFixed(2))
+
+const subscriber = 10000
+console.log(subscriber.toLocaleString())
+
+console.log(Math.random()*10);
+
+console.log(Math.floor(Math.random()*10));
+
+// () --> paranthesis
+// [] -->  thesis
+// {} --> curly brasis
+

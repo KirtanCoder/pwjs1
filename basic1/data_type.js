2f71fafc4ee19c54a180ac74e2fbc9c4e233ad38
KirtanCoder <kirtankrpc@gmail.com>
2024-07-05 07:14:22 +0000

    discussion on data types

---
+++ basic1/data_type.js
@@ -0,0 +1,57 @@
+//Primitive (stack)
+
+/* String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+ BigInt 
+ */
+const firstname = "Kirtan"
+const age = 20
+const score = 200
+let isloggedIn = true
+const nodeVersion = null
+let bankBalance = undefined
+const buttonId = symbol("id")
+const aReallyLongerNumber = 903883894n
+
+//Reference(Non-primitive) - Objects (Heap)
+
+/*
+
+Array 
+objects
+functions
+
+*/
+//Array
+const numbers = [1,2,3]
+const heroes = [
+    "superman",
+    "batman",
+    "flashman",
+    true,
+    1
+]
+//Objects
+const userobject = {
+    name: "hitesh",
+    age:20,
+    isLoggedIn: true
+}
+
+
+
+
+//function
+const sayhello = function() {
+    console.log("kirtan")
+}
+
+const Registeruser = function() {
+
+}
+
+const loginuser = function() {}

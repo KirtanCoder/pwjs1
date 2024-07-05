df7e8ae4ca6c41fce98082e7c518381636a1d0b6
KirtanCoder <kirtankrpc@gmail.com>
2024-07-05 18:58:18 +0000

    learned objects

---
+++ basic1/objects.js
@@ -0,0 +1,34 @@
+// {} --> objects
+
+const user = { 
+    username: "hc",
+    name: "kirtan",
+email: "kirt@.com",
+age : 20,
+isAdmin: true,
+isLoggedIn: true,
+
+tea: ["lemon","peach","ginger"],
+
+address: {
+    city:"jaipur",
+    state: "Rajasthan"
+
+}
+}
+const anotheruser = user;
+
+
+console.log(user.address.state)
+console.log(user.age)
+
+console.log(user.tea[1])
+
+console.log(user["address"]["state"])
+user.email = "kirtankrpc"
+console.log(user)
+
+console.log(anotheruser)
+
+delete user.isAdmin
+console.log(user)

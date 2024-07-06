ec80066f8eb283532d0884bcafeb47b8dcf40335
KirtanCoder <kirtankrpc@gmail.com>
2024-07-06 04:12:40 +0000

    new codes

--- basic1/objects.js
+++ basic1/objects.js
@@ -19,16 +19,30 @@ address: {
 const anotheruser = user;
 
 
-console.log(user.address.state)
-console.log(user.age)
+// console.log(user.address.state)
+// console.log(user.age)
 
-console.log(user.tea[1])
+// console.log(user.tea[1])
 
-console.log(user["address"]["state"])
-user.email = "kirtankrpc"
-console.log(user)
+// console.log(user["address"]["state"])
+// user.email = "kirtankrpc"
+// console.log(user)
 
-console.log(anotheruser)
+// console.log(anotheruser)
 
-delete user.isAdmin
-console.log(user)
+// delete user.isAdmin
+// console.log(user)
+
+const userlistfromdatabase = [{},{},{}]
+
+console.log(Object.keys(user))
+
+
+console.log(Object.values(user))
+
+
+console.log(Object.entries(user))
+
+// console.log(user.keyboard) -->  UNDEFINED
+
+console.log(user.hasOwnProperty('tea'))

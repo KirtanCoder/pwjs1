718a37f42ca5aec979f430f214a3ed59b9dfae58
KirtanCoder <kirtankrpc@gmail.com>
2024-07-11 07:09:36 +0000

    added new code

---
+++ control2/switch.js
@@ -0,0 +1,20 @@
+let getstarfromuser = "2"
+
+let starinnumber =parseInt(getstarfromuser)
+
+
+switch (starinnumber) {
+    case 1:
+        console.log("poor");
+        break;
+
+
+        case 2:
+            console.log("rich");
+            // break;
+
+    default:
+
+    console.log("not matched");
+        break;
+}

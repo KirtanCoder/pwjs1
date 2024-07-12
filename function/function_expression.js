865c0e41893a90544a5196609cf22cd2d63476d2
KirtanCoder <kirtankrpc@gmail.com>
2024-07-12 17:20:21 +0000

    some changes

---
+++ function/function_expression.js
@@ -0,0 +1,36 @@
+function fun(){  // function declration
+
+}
+//........
+const f = function process() { // named function
+
+}
+//..............
+const g = function () { // anonymous  function
+
+}
+//...................
+const h = () => {
+
+}
+//..............................
+(function () {
+
+})()
+//...........................
+(function exec()    {
+
+}
+)
+
+
+//why named function expre important?
+
+//1. debugging -> Finding error in your code
+//2. Recursion
+// 3. Reusability
+
+
+//console.trace("using trace we can print the order in which function is called")
+
+// callstack ->  

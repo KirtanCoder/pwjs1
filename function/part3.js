f6d48fcbc2eca21f4f192e3454c58fe3873e7202
KirtanCoder <kirtankrpc@gmail.com>
2024-07-17 08:01:55 +0000

    IIFE

--- function/part3.js
+++ function/part3.js
@@ -1,12 +1,41 @@
 
-function fun() {
-    console.log("Hello world");
+// function fun() {
+//     console.log("Hello world");
 
-  return  function cleanup() {
-        console.log("Cleaning up the resource");
-    }
+//   return  function cleanup() {
+//         console.log("Cleaning up the resource");
+//     }
+// }
+
+// const result = fun();
+// console.log(result);
+// result();
+
+
+// IIFE -> immediately invoked funtion expression
+//  -> 
+let variable = 10;
+
+function f() {
+    console.log(variable);
 }
 
-const result = fun();
-console.log(result);
-result();
+f();
+
+
+// intern by mistake makes the same function
+
+function downloadscript() {
+    console.log("New function created");
+}
+
+//intern by mistake makes the same function
+// To solve this, we can wrap the Whole intern`s code that can break, into an IIFE
+
+(function () {
+    function downloadscriptfromideone() {
+        console.log("New function Created");
+    }
+    console.log("calling intern code")
+    downloadscriptfromideone();
+})();

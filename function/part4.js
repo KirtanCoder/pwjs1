63b9f60217a26f2b4b4e7a3b3bf7ac2da3e1e08c
KirtanCoder <kirtankrpc@gmail.com>
2024-07-18 18:45:59 +0000

    scopes

---
+++ function/part4.js
@@ -0,0 +1,19 @@
+// SCopes
+// -> scopes define visibility
+
+// it dtermines where a variable is visible
+// is javascript is interprated language? --> when execution happen line by line
+//=> bash
+// compiled language==>  c/c++
+//Hybrid language => java,python,js
+
+
+
+//js executes your code in 3 phases:
+
+//1st phase --> compilation ->1.syntax error / 2.scope resolution
+//2nd phase --> execution --> 1.value assignment to variable / 2.code execution
+
+// how to stop auto global auto creation
+
+"use strict"; //This enables the scrict mode

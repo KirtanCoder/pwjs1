e3a87bec2889ef3923dd47c984ee975eab1832ac
KirtanCoder <kirtankrpc@gmail.com>
2024-07-25 18:10:19 +0000

    concatenate and decimal to binary

--- function/part7.js
+++ function/part7.js
@@ -1,9 +1,22 @@
 //Lambda function -> Arrow function () => {}
 
-    function processarray(array , fn) {
-        for(let i=0;i<array.length;i++) {
-            console.log("index",i,"value",array[i],"return value of fn",fn(array[i]));
-        }
-    }
-    processarray([2,3,4,5,6], (x) => {return x*x*x;});
-    processarray([2,3,4,5,6], x => x*x*x);
+    // function processarray(array , fn) {
+    //     for(let i=0;i<array.length;i++) {
+    //         console.log("index",i,"value",array[i],"return value of fn",fn(array[i]));
+    //     }
+    // }
+    // processarray([2,3,4,5,6], (x) => {return x*x*x;});
+    // processarray([2,3,4,5,6], x => x*x*x);
+
+
+//concatenation
+
+// let x = 'sea'
+// let y="twit"
+// let result = x.concat(y);
+// console.log(result);
+
+//convert decimal to binary
+
+const x= 13;
+console.log(x.toString(2));
